badb88151f46917c63566566328918d8b57da5c5
jackson ford <jfordnose64@gmail.com>
2019-11-30 11:44:42 -0500

    added api call

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
 
 const Home = () => {
-  return <div>Wow</div>
+  return (
+    <div>
+      <h1>Superheroes</h1>
+    </div>
+  )
 }
 
 export default Home
--- src/components/SuperHeroes.jsx
+++ src/components/SuperHeroes.jsx
@@ -1,8 +1,41 @@
 import React, { useState, useEffect } from 'react'
 import Axios from 'axios'
+import { Button } from 'reactstrap'
 
 const SuperHeroes = () => {
-  return <div>wow</div>
+  const [character, setCharacter] = useState('Batman')
+  const [secondCharacter, setSecondCharacter] = useState('')
+  const [characterOneData, setCharacterOne] = useState([])
+
+  const getSuperHeroes = async () => {
+    const apiKey = '2582138012013683'
+
+    const resp = await Axios(
+      `https://superheroapi.com/api/${apiKey}/search/${character}`
+    )
+    console.log(resp.data)
+    console.log(character)
+    setCharacterOne(resp.data)
+  }
+
+  // useEffect(() => {
+  //   getSuperHeroes()
+  // }, [])
+
+  return (
+    <div>
+      <input
+        type="text"
+        value={character}
+        onChange={e => setCharacter(e.target.value)}
+      />
+      <h1>Hello</h1>
+      <Button color="primary" onClick={() => getSuperHeroes()}>
+        Enter
+      </Button>
+      <h2>{character.id}</h2>
+    </div>
+  )
 }
 
 export default SuperHeroes
